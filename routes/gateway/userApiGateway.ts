def279ca5c207018644f4c4065a122ba11a0af4e
Bhuvaneshwara Raja <bhuvaneshraja113@gmail.com>
2023-02-04 18:56:50 +0530

    Add Dockerfile, Request Adaptor, router

--- routes/gateway/userApiGateway.ts
+++ routes/gateway/userApiGateway.ts
@@ -2,7 +2,7 @@ const apiExpress = require('express');
 
 const apiRouter = apiExpress.Router();
 
-apiRouter.get('/test', (req,res) => {
-    res.send('user api works !!!');
+apiRouter.get('/login', (req:any,res:any) => {
+    res.send("user api works");
 })
 module.exports = apiRouter;
--- routes/index.ts
+++ routes/index.ts
@@ -2,6 +2,7 @@ const nodeExpress = require('express');
 
 const router = nodeExpress.Router();
 
-router.use("/user",require('./api/userApi.ts'))
+router.use("/user",require('./gateway/userApiGateway.ts'))
+
 
 module.exports = router;
